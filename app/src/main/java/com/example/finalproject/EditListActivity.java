9410eee31549e15207f93f5acff5598fb41237a4
iris grabois <irisgrabois@gmail.com>
2022-07-06 20:09:54 +0300

    dialog for new item+ recycler view

--- app/src/main/java/com/example/finalproject/EditListActivity.java
+++ app/src/main/java/com/example/finalproject/EditListActivity.java
@@ -1,15 +1,43 @@
 package com.example.finalproject;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EditListActivity extends AppCompatActivity {
-
+    Button addBtn ;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addBtn = findViewById(R.id.add_btn);
 
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //System.out.println("Click");
+                AlertDialog.Builder builder = new AlertDialog.Builder(EditListActivity.this);
+                View mView =  getLayoutInflater().inflate(R.layout.dialog, null);
+                builder.setCancelable(false);
+                builder.setView(mView);
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+
+
+
+        });
     }
 
 }
